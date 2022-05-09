b2b4f1fb4f074dcfadff988dd38227d964dc454b
dasunikawijesooriya <74725211+dasunikawijesooriya@users.noreply.github.com>
2022-05-09 22:32:08 +0530

    Frontend

---
+++ app/src/main/java/com/example/accommodations_management/adddata.java
@@ -0,0 +1,88 @@
+package com.example.accommodations_management;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class adddata extends AppCompatActivity
+{
+    EditText name, location,discription,venue,contact,surl;
+    Button submit,back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adddata);
+
+        name=(EditText)findViewById(R.id.add_name);
+        location=(EditText)findViewById(R.id.add_location);
+        discription=(EditText)findViewById(R.id.add_discription);
+        venue=(EditText)findViewById(R.id.add_venue);
+        contact=(EditText)findViewById(R.id.add_contact);
+        surl=(EditText)findViewById(R.id.add_surl);
+
+        back=(Button)findViewById(R.id.add_back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Dashboard.class));
+                finish();
+            }
+        });
+
+        submit=(Button)findViewById(R.id.add_submit);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                processinsert();
+            }
+        });
+    }
+
+    private void processinsert()
+    {
+        Map<String,Object> map=new HashMap<>();
+        map.put("name",name.getText().toString());
+        map.put("location",location.getText().toString());
+        map.put("discription",discription.getText().toString());
+        map.put("venue",venue.getText().toString());
+        map.put("contact",contact.getText().toString());
+        map.put("surl",surl.getText().toString());
+        FirebaseDatabase.getInstance().getReference().child("hotel").push()
+                .setValue(map)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        name.setText("");
+                        location.setText("");
+                        discription.setText("");
+                        venue.setText("");
+                        contact.setText("");
+                        surl.setText("");
+                        Toast.makeText(getApplicationContext(),"Inserted Successfully",Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e)
+                    {
+                        Toast.makeText(getApplicationContext(),"Could not insert",Toast.LENGTH_LONG).show();
+                    }
+                });
+
+    }
+}
